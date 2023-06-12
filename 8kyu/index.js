bf2d27f12077b157a7806859d75ef99e03c007d0
Doston <1764rajabovv@gmail.com>
2023-06-12 01:20:27 -0700

    started 8kyu exercises

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -2,10 +2,10 @@
 Reversed Strings
 Complete the solution so that it reverses the string passed into it.
 */
-// function solution(str) {
-//   return str.split("").reverse().join("");
-// }
-// console.log(solution("hello world"));
+function solution(str) {
+  return str.split("").reverse().join("");
+}
+console.log(solution("hello world"));
 
 // ==================================
 
