84ad7190f5d19b3dc697f4e788edc5aa7e261d5a
Doston <1764rajabovv@gmail.com>
2023-06-13 00:47:35 -0700

    today I did 7 exercises on codewars

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -8,4 +8,66 @@ Complete the solution so that it reverses the string passed into it.
 // console.log(solution("hello world"));
 
 // ==================================
+// Remove First and Last Character
 
+// function removeChar(str) {
+//   return str.split("").slice(1,-1).join("");
+// }
+// console.log(removeChar("football"))
+
+// ====================================
+
+// String repeat
+
+// function repeatStr (n, s) {
+//   return s.repeat(n);
+// }
+
+// console.log(repeatStr(5,"hello"));
+
+// ====================================
+
+// Remove String Spaces
+
+// function noSpace(x){
+// return x.split(" ").join("")
+// }
+// console.log(noSpace("     hello world"));
+
+// =======================================
+
+// convert a string to a number
+
+// const stringToNumber = function(str){
+//   // put your code here
+//   return Number(str);
+// }
+// console.log(stringToNumber("12345"));
+// =======================================
+
+// convert a boolean to a string
+
+// function booleanToString(b){
+//   return String(b)
+// }
+// console.log(booleanToString(true));
+
+// =====================================
+
+// reversed words Complete the solution so that it reverses all of the words within the string passed in.
+// function reverseWords(str) {
+//   return (newArr = str.split(" ").reverse().join(" "));
+//   // let newArr = str.reverse().join("")
+// }
+
+// console.log(reverseWords("hello world and welcome to my codewars profile!"));
+
+// =========================================
+
+// make uppercase
+
+// function makeUpperCase(str) {
+//   return str.toUpperCase()
+// }
+
+// console.log(makeUpperCase("hello"));
