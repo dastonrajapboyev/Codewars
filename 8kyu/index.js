7e4f5272b9bb9c7f5230e0d13fd638668bed79a2
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-07-19 23:59:23 -0700

    exercises: L1: Bartender, drinks!, Welcome to the City

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -667,3 +667,83 @@ Complete the solution so that it reverses the string passed into it.
 // console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois'));
 
 // ==============================================
+
+// Training JS #1: create your first JS function and print "Hello World!"
+
+// function helloWorld  (){
+//   let str = "Hello World!"
+//   console.log(str);
+// }
+// helloWorld();
+
+// ====================================
+
+// let v1 = 50,
+//   v2 = 100,
+//   v3 = 150,
+//   v4 = 200,
+//   v5 = 2,
+//   v6 = 250;
+
+// function equal1() {
+//   let a = v1,
+//     b = v1;
+//   return a + b;
+// }
+// function equal2() {
+//   let a = v6,
+//     b = v3;
+//   return a - b;
+// }
+
+// function equal3() {
+//   let a = v1,
+//     b = v5;
+//   return a * b;
+// }
+// function equal4() {
+//   let a = v4,
+//     b = v5;
+//   return a / b;
+// }
+
+// function equal5() {
+//   let a = v2,
+//     b = v4;
+//   return a % b;
+// }
+
+//=======================================
+
+var a1 = "A",
+  a2 = "a",
+  b1 = "B",
+  b2 = "b",
+  c1 = "C",
+  c2 = "c",
+  d1 = "D",
+  d2 = "d",
+  e1 = "E",
+  e2 = "e",
+  n1 = "N",
+  n2 = "n";
+function Dad() {
+  return d1 + a2 + d2;
+}
+function Bee() {
+  return b1 + e2 + e2;
+}
+function banana() {
+  return b2 + a2 + n2 + a2 + n2 + a2;
+}
+// //answer some questions if you finished works above
+function answer1(){
+  return "yes";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+function answer3(){
+  return "yes";
+}
