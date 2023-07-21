c02fcc90177f8b5a81d3edd24dc08bad5c035478
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-07-21 00:36:39 -0700

    exercise: Training JS #8: Conditional statement -- switch

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -803,3 +803,32 @@ Complete the solution so that it reverses the string passed into it.
 // console.log(saleHotdogs(100));
 
 // ===========================================
+
+// Training JS #8: Conditional statement--switch
+
+// function howManydays(month) {
+//   var days;
+//   switch (month) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//       days = 31;
+//       break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//       days = 30;
+//       break;
+//     case 2:
+//       days = 28;
+//   }
+//   return days;
+// }
+// console.log(howManydays(2));
+
+// ========================================
