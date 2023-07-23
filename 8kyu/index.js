230dc7c7ca073b463da6dc6d63fcb4ffecc8345e
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-07-23 00:28:26 -0700

    exercise: Training JS #11: loop statement --break,continue

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -868,3 +868,27 @@ Complete the solution so that it reverses the string passed into it.
 // console.log(pickIt([1,2,3]));
 
 // ===================================
+
+// Training JS #11: loop statement --break,continue
+
+// function grabDoll(dolls) {
+//   var bag = [];
+//   for (let i = 0; i < dolls.length; i++) {
+//     if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") bag.push(dolls[i]);
+//     else continue
+//     if (bag.length === 3) break;
+//   }
+//   return bag;
+// }
+// console.log(
+//   grabDoll([
+//     "Mickey Mouse",
+//     "Hello Kitty",
+//     "Hello Kitty",
+//     "Hello Kitty",
+//     "Hello Kitty",
+//     "Snow white",
+//   ])
+// );
+
+// ======================================
