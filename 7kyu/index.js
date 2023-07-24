f20ece46114679a993f112794f2740d5365021f7
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-07-23 21:49:59 -0700

    exercises: Help the Fruit Guy, Sort by Last Char, Holiday III - Fire on the boat

--- 7kyu/index.js
+++ 7kyu/index.js
@@ -10,3 +10,37 @@
 // console.log(capitals("CodEWaRs"));
 
 // ===================================
+
+// Help the Fruit Guy
+
+// function removeRotten(bagOfFruits){
+//   let arr = []
+//   if(bagOfFruits === null) return arr
+//   return (bagOfFruits || []).map((val)=>{
+//     return val.includes("rotten") ? val.slice(6).toLowerCase() : val
+//   })
+// }
+// console.log(removeRotten(null));
+
+// ====================================
+
+// Sort by Last Char
+
+// function last(x){
+//   x=x.split(" ")
+//   return x.sort((a,b)=>{
+//     return a[a.length-1].localeCompare(b[b.length-1])
+//   })
+// }
+// console.log(last('take me to semynak'));
+
+// ===================================
+
+// Holiday III - Fire on the boat
+
+// function fireFight(s){
+//   return s.replace(/Fire/g,"~~")
+// }
+// console.log(fireFight("Mast Deck Engine Water Fire"));
+
+// ========================================
