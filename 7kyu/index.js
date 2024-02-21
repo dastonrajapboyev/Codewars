8858989dfcb682219112ab7348bc3ecdad089c60
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2024-02-21 22:50:38 +0500

    7 kyu // Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe and Coding Meetup #2 - Higher-Order Functions Series - Greet developers solved

--- 7kyu/index.js
+++ 7kyu/index.js
@@ -150,3 +150,102 @@
 // console.log(modifyMultiply("This is a string", 3, 5));
 
 // ===================================
+
+// 7 kyu
+// Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe
+
+// function countDevelopers(list) {
+//   return list.reduce(
+//     (count, curr) =>
+//       count +
+//       (curr.continent === "Europe" && curr.language === "JavaScript" ? 1 : 0),
+//     0
+//   );
+// }
+// const list1 = [
+//   {
+//     firstName: "Noah",
+//     lastName: "M.",
+//     country: "Switzerland",
+//     continent: "Europe",
+//     age: 19,
+//     language: "JavaScript",
+//   },
+//   {
+//     firstName: "Doston",
+//     lastName: "M.",
+//     country: "Switzerland",
+//     continent: "Europe",
+//     age: 19,
+//     language: "JavaScript",
+//   },
+//   {
+//     firstName: "Maia",
+//     lastName: "S.",
+//     country: "Tahiti",
+//     continent: "Oceania",
+//     age: 28,
+//     language: "JavaScript",
+//   },
+//   {
+//     firstName: "Shufen",
+//     lastName: "L.",
+//     country: "Taiwan",
+//     continent: "Asia",
+//     age: 35,
+//     language: "HTML",
+//   },
+//   {
+//     firstName: "Sumayah",
+//     lastName: "M.",
+//     country: "Tajikistan",
+//     continent: "Asia",
+//     age: 30,
+//     language: "CSS",
+//   },
+// ];
+
+// console.log(countDevelopers(list1));
+
+// ===========================================
+
+// 7 kyu
+// Coding Meetup #2 - Higher-Order Functions Series - Greet developers
+
+// function greetDevelopers(list) {
+//   return list.map((value) => {
+//     return {
+//       ...value,
+//       greeting: `Hi ${value.firstName}, what do you like the most about ${value.language}?`,
+//     };
+//   });
+// }
+
+// const list1 = [
+//   {
+//     firstName: "Sofia",
+//     lastName: "I.",
+//     country: "Argentina",
+//     continent: "Americas",
+//     age: 35,
+//     language: "Java",
+//   },
+//   {
+//     firstName: "Lukas",
+//     lastName: "X.",
+//     country: "Croatia",
+//     continent: "Europe",
+//     age: 35,
+//     language: "Python",
+//   },
+//   {
+//     firstName: "Madison",
+//     lastName: "U.",
+//     country: "United States",
+//     continent: "Americas",
+//     age: 32,
+//     language: "Ruby",
+//   },
+// ];
+
+// console.log(greetDevelopers(list1));
