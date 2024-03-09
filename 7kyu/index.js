a31e33b3eddb92859b1a038393d538aa353dbec3
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2024-03-09 23:51:22 +0500

    7 kyu // Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?

--- 7kyu/index.js
+++ 7kyu/index.js
@@ -381,3 +381,42 @@
 // console.log(countLanguages(list1));
 
 // ================================
+
+// 7 kyu
+// Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?
+
+// function isSameLanguage(list) {
+//   const firstLanguage = list[0].language;
+//   return list.every((value) => value.language === firstLanguage);
+// }
+
+// var list1 = [
+//   {
+//     firstName: "Daniel",
+//     lastName: "J.",
+//     country: "Aruba",
+//     continent: "Americas",
+//     age: 42,
+//     language: "JavaScript",
+//   },
+//   {
+//     firstName: "Kseniya",
+//     lastName: "T.",
+//     country: "Belarus",
+//     continent: "Europe",
+//     age: 22,
+//     language: "JavaScript",
+//   },
+//   {
+//     firstName: "Hanna",
+//     lastName: "L.",
+//     country: "Hungary",
+//     continent: "Europe",
+//     age: 65,
+//     language: "JavaScript",
+//   },
+// ];
+
+// console.log(isSameLanguage(list1));
+
+// ======================================
